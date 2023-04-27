b6d6a0fcbda289c75e54b639ce32e19aa4cfafa6
MM <cxultimate@gmail.com>
2023-04-27 21:15:26 +0200

    Delete old code

--- IdentityServer_Poseify/Pages/Account/Logout/LogoutOptions.cs
+++
@@ -1,8 +0,0 @@
-
-namespace IdentityServer_Poseify.Pages.Logout;
-
-public class LogoutOptions
-{
-    public static bool ShowLogoutPrompt = true;
-    public static bool AutomaticRedirectAfterSignOut = false;
-}
